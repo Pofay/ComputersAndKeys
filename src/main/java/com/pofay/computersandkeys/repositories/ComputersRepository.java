deab5d5cef27f61ed15178eefc180ba06b590f21
Pofay <gianCarlo.gilos@gmail.com>
2021-04-15 22:54:21 +0800

    Cleaning up

--- src/main/java/com/pofay/computersandkeys/repositories/ComputersRepository.java
+++ src/main/java/com/pofay/computersandkeys/repositories/ComputersRepository.java
@@ -1,9 +1,7 @@
 package com.pofay.computersandkeys.repositories;
 
 import com.pofay.computersandkeys.entities.Computer;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface ComputersRepository extends CrudRepository<Computer, String> {
-
 }
