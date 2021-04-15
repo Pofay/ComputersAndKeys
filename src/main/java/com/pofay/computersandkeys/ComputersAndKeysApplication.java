0d870222799e1bd2a7efab996710ae9bb5141fd5
Pofay <gianCarlo.gilos@gmail.com>
2021-04-15 21:31:16 +0800

    Provided @ComponentScan to prevent empty dependencies during autowire

--- src/main/java/com/pofay/computersandkeys/ComputersAndKeysApplication.java
+++ src/main/java/com/pofay/computersandkeys/ComputersAndKeysApplication.java
@@ -3,10 +3,12 @@ package com.pofay.computersandkeys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan("com.pofay.computersandkeys")
 @EnableWebMvc
 public class ComputersAndKeysApplication {
 
