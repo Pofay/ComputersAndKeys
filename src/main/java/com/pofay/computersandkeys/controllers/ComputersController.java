d3104d4e570aafae468838c738fbe79ab1d8795b
Pofay <gianCarlo.gilos@gmail.com>
2021-04-15 22:54:03 +0800

    Added another route to query all models by maker

--- src/main/java/com/pofay/computersandkeys/controllers/ComputersController.java
+++ src/main/java/com/pofay/computersandkeys/controllers/ComputersController.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.pofay.computersandkeys.entities.Computer;
-import com.pofay.computersandkeys.repositories.ComputersRepository;
 import com.pofay.computersandkeys.services.ComputersService;
+import com.pofay.computersandkeys.utils.ResponseFormatter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -35,31 +35,20 @@ public class ComputersController {
 
         if (computerOrEmpty.isPresent()) {
             Computer c = computerOrEmpty.get();
-            JSONObject response = composeResponse(c);
-            System.out.println(response.toString());
+            JSONObject response = ResponseFormatter.composeResponse(c);
             return ResponseEntity.ok().body(response.toString());
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    private JSONObject composeResponse(Computer c) throws JSONException {
-        JSONObject color = new JSONObject();
-        color.put("color", c.getColors());
-        JSONObject computerDetails = toJSON(c);
-        computerDetails.put("colors", color);
-        JSONObject response = new JSONObject();
-        response.put("computer", computerDetails);
-        return response;
+    @GetMapping(value = "/computers/{maker}")
+    public ResponseEntity getComputersByMaker(@PathVariable("maker") String maker, HttpServletRequest req,
+            HttpServletResponse res) {
+        if (service.hasAssociatedModels(maker)) {
+            return ResponseEntity.status(403).build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-
-    private JSONObject toJSON(Computer c) throws JSONException {
-        JSONObject computerDetails = new JSONObject();
-        computerDetails.put("maker", c.getMaker());
-        computerDetails.put("language", c.getLanguage());
-        computerDetails.put("model", c.getModelNumber());
-        computerDetails.put("type", c.getType());
-        return computerDetails;
-    }
-
 }
