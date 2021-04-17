174f60d722d964fceae6791b143b164462c09b6a
Pofay <gianCarlo.gilos@gmail.com>
2021-04-17 16:27:43 +0800

    Composed Request body in single file

--- src/main/java/com/pofay/computersandkeys/requests/AddSSHKeyRequest.java
+++ src/main/java/com/pofay/computersandkeys/requests/AddSSHKeyRequest.java
@@ -1,5 +1,10 @@
 package com.pofay.computersandkeys.requests;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -7,9 +12,22 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode
 public class AddSSHKeyRequest {
 
-    private SSHKeyDetails sshkey;
+    @JsonProperty("ssh-key")
+    private SshKey sshkey;
 
     public AddSSHKeyRequest() {
     }
 
+    @Data
+    @EqualsAndHashCode
+    public static class SshKey {
+        private String type;
+        @JsonProperty("public")
+        private String publicKey;
+        private String comment;
+
+        public SshKey() {
+        }
+
+    }
 }
