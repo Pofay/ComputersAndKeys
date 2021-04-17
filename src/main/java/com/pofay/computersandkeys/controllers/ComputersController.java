03eb698cea86c14add4c2b5301324f3cc59bab06
Pofay <gianCarlo.gilos@gmail.com>
2021-04-17 12:12:41 +0800

    Created new method instead to deal with clients requeting xml from route

--- src/main/java/com/pofay/computersandkeys/controllers/ComputersController.java
+++ src/main/java/com/pofay/computersandkeys/controllers/ComputersController.java
@@ -12,6 +12,7 @@ import com.pofay.computersandkeys.services.ComputersService;
 import com.pofay.computersandkeys.utils.ResponseFormatter;
 
 import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,11 +28,19 @@ public class ComputersController {
         this.service = service;
     }
 
-    @GetMapping(value = "/computers/{maker}/{model_number}", produces = { "application/json", "application/xml" })
-    public ResponseEntity getComputerModel(@PathVariable("maker") String maker,
+    @GetMapping(value = "/computers/{maker}/{model_number}", produces = "application/json")
+    public ResponseEntity getComputerModelJSON(@PathVariable("maker") String maker,
             @PathVariable("model_number") String modelNumber, HttpServletRequest req, HttpServletResponse res) {
         return service.findMatchingComputerByMakerAndModel(maker, modelNumber).map(ResponseFormatter::composeResponse)
                 .map(r -> ResponseEntity.ok().body(r.toString())).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(value = "/computers/{maker}/{model_number}", produces = "application/xml")
+    public ResponseEntity getComputerModelXML(@PathVariable("maker") String maker,
+            @PathVariable("model_number") String modelNumber, HttpServletRequest req, HttpServletResponse res) {
+        return service.findMatchingComputerByMakerAndModel(maker, modelNumber).map(ResponseFormatter::composeResponse)
+                .map(r -> XML.toString(r)).map(r -> ResponseEntity.ok().body(r))
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping(value = "/computers/{maker}")
