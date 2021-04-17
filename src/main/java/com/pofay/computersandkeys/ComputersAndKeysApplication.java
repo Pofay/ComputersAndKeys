eeb4dba64ef0fe7e0f2e2ffacd45c175623568a9
Pofay <gianCarlo.gilos@gmail.com>
2021-04-17 21:23:12 +0800

    Added dummy data during startup for computers table

--- src/main/java/com/pofay/computersandkeys/ComputersAndKeysApplication.java
+++ src/main/java/com/pofay/computersandkeys/ComputersAndKeysApplication.java
@@ -1,8 +1,17 @@
 package com.pofay.computersandkeys;
 
+import java.util.Arrays;
+import java.util.UUID;
+
+import com.pofay.computersandkeys.entities.Computer;
+import com.pofay.computersandkeys.entities.ComputerTypes;
+import com.pofay.computersandkeys.repositories.ComputersRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -14,6 +23,17 @@ public class ComputersAndKeysApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComputersAndKeysApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(ComputersRepository repo) {
+
+		return (args) -> {
+			repo.deleteAll();
+			Computer c = new Computer("X507UA", "ASUS", ComputerTypes.LAPTOP, "日本語", Arrays.asList("black", "silver"));
+			repo.save(c);
+		};
+
 	}
 
 }
