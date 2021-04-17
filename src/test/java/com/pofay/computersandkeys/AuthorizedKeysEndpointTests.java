5333403062d9c64205da825c1ff873bf0d535e9c
Pofay <gianCarlo.gilos@gmail.com>
2021-04-17 14:53:01 +0800

    Restructured first test to deal with 400 on empty request and 201 on full body in second test

--- src/test/java/com/pofay/computersandkeys/AuthorizedKeysEndpointTests.java
+++ src/test/java/com/pofay/computersandkeys/AuthorizedKeysEndpointTests.java
@@ -7,6 +7,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import net.minidev.json.JSONObject;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,10 +34,23 @@ public class AuthorizedKeysEndpointTests {
     }
 
     @Test
-    public void post_using_full_endpoint_returns_201() throws Exception {
+    public void post_without_body_returns_400() throws Exception {
 
         mvc.perform(post("/build-server/jenkins/authorized_keys").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
-
+                .andExpect(status().isBadRequest());
     }
+
+    @Test
+    public void post_with_body_returns_201() throws Exception {
+        JSONObject subPayload = new JSONObject();
+        subPayload.put("type", "ssh-rd25519");
+        subPayload.put("public", "AAAAC3NzaC1lZDI1NTE5AAAAIOiKKC7lLUcyvJMo1gjvMr56XvOq814Hhin0OCYFDqT4");
+        subPayload.put("comment", "happy@isr");
+        JSONObject payload = new JSONObject();
+        payload.put("ssh-key", subPayload);
+
+        mvc.perform(post("/build-server/jenkins/authorized_keys").contentType(MediaType.APPLICATION_JSON)
+                .content(payload.toString())).andExpect(status().isCreated());
+    }
+
 }
