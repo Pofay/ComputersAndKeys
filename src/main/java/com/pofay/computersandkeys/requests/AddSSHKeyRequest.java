c64c2f0738882d98b3e7b4e498c55ee9a35f019c
Pofay <gianCarlo.gilos@gmail.com>
2021-04-18 17:02:30 +0800

    Removed unused packages

--- src/main/java/com/pofay/computersandkeys/requests/AddSSHKeyRequest.java
+++ src/main/java/com/pofay/computersandkeys/requests/AddSSHKeyRequest.java
@@ -1,9 +1,6 @@
 package com.pofay.computersandkeys.requests;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
