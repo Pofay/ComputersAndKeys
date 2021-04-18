6933acc5beeaf644098aafed856e44b1f4825c0f
Pofay <gianCarlo.gilos@gmail.com>
2021-04-18 21:10:04 +0800

    Added direct app name in config

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,6 +3,15 @@
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 sudo apt-get update
 
-sudo echo -e "machine api.heroku.com \n  login $HEROKU_LOGIN\n  password $HEROKU_API_KEY\nmachine git.heroku.com\n  login $HEROKU_LOGIN\n  password $HEROKU_API_KEY" >> ~/.netrc
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+machine git.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
  
 heroku git:remote -a $HEROKU_APP
